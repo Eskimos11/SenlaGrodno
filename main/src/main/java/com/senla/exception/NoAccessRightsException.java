7977b0536f0ec0f937c5330c236ec3410cd09aee
pavel <pavel_kurilo@mail.ru>
2022-01-26 02:31:23 +0300

    update

---
+++ main/src/main/java/com/senla/exception/NoAccessRightsException.java
@@ -0,0 +1,12 @@
+package com.senla.exception;
+
+import lombok.Getter;
+
+public class NoAccessRightsException extends RuntimeException{
+    @Getter
+    private String message;
+
+    public NoAccessRightsException(String message) {
+        this.message = message;
+    }
+}
